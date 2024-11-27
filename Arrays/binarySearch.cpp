0a9da035a2a89bae0784c8f66361fe5c3aba473a
surabhi555 <pednekarsurabhi555@gmail.com>
2024-11-27 15:24:02 +0530

    binary search

---
+++ Arrays/binarySearch.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+int binarySearch(int arr[],int n,int key){
+    int start=0;
+    int end=n-1;
+    int mid=(start+end)/2;
+   while(start<end){
+         if(arr[mid]==key){
+        return mid;
+            }
+    else if(arr[mid]>key){
+       start=mid+1;
+       mid=(start+end)/2;
+
+    }
+    else{
+        end=mid-1;
+        mid=(start+end)/2;
+    }
+   }
+
+}
+void printArray(int arr[],int n){
+    for(int i=0;i<n;i++){
+        cout << arr[i] <<" ";
+    }
+    cout <<endl;
+}
+
+int main(){
+    int even[10]={20,40,60,70,80,90,100,120,130,140};
+    int key;
+    cin >> key;
+    binarySearch(even,10,key);
+    printArray(even,10);
+    return 0;
+}
