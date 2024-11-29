dc23986c8ab2171a3cad52855e0d74b8769a14b8
surabhi555 <pednekarsurabhi555@gmail.com>
2024-11-29 10:57:53 +0530

    book allocation problem

---
+++ BINARY_SEARCH/bookAllocation.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool isPossible(int arr[],int n,int m,int mid){
+    int studentCount=1;
+    int pageSum=0;
+        for( int i=0;i<n;i++){
+                if(pageSum+arr[i] <= mid){
+                    pageSum+=arr[i];
+                }
+                else{
+                    studentCount++;
+                    if(studentCount > m || arr[i] > mid){
+                        return false;
+                    }
+                    pageSum=arr[i];
+                }
+        }
+        return true;
+}
+
+
+int allocateBooks(int arr[],int n,int m){
+    int s=0;
+    int sum=0;
+    int ans=-1;
+    for(int i=0;i<n;i++){
+        sum+=arr[i];
+    }
+    int e=sum;
+    int mid=s+(e-s)/2;
+        while( s <= e){
+            if(isPossible(arr,n,m,mid)){
+                ans=mid;
+                e=mid-1;
+            }
+            else{
+                s=mid+1;
+            }
+            mid=s+(e-s)/2;
+        }
+        return ans;
+
+
+}
+
+int main(){
+    int arr[5]={7,2,5,10,8};
+    int m=2;
+    int n=5;
+    int pages=allocateBooks(arr,5,2);
+    cout << "pages allocated " << pages<<endl;
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,7 +1,3 @@
-#DAY 4
+#DAY 5
 #SOLVED
-binary search apllications
-first and last occurence in array
-peak index in mountain array
-pivot element in array
-search in sorted and rotated array
+book allocation problem
