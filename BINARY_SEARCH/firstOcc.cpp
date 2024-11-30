23a8f7ace81dae5efb16c7640390a6c97ce0cf3b
surabhi555 <pednekarsurabhi555@gmail.com>
2024-11-30 21:33:35 +0530

    done

--- BINARY_SEARCH/firstOcc.cpp
+++ BINARY_SEARCH/firstOcc.cpp
@@ -1,3 +1,13 @@
+/* You are given an integer mountain array arr of length n where the values increase to a peak element and then decrease.
+
+Return the index of the peak element.
+
+Your task is to solve it in O(log(n)) time complexity.
+*/
+
+
+
+
 #include<bits/stdc++.h>
 using namespace std;
 int firstOcc(int arr[],int n,int key){
