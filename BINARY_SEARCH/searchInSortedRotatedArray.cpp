c5d1b5c3b7a21fd1caa42feb904ae677488bea07
surabhi555 <pednekarsurabhi555@gmail.com>
2024-11-30 21:40:23 +0530

    done

--- BINARY_SEARCH/searchInSortedRotatedArray.cpp
+++ BINARY_SEARCH/searchInSortedRotatedArray.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+/*You have been given a sorted array/list 'arr' consisting of ‘n’ elements. You are also given an integer ‘k’.
+
+
+
+Now the array is rotated at some pivot point unknown to you.
+
+
+
+For example, if 'arr' = [ 1, 3, 5, 7, 8], then after rotating 'arr' at index 3, the array will be 'arr' = [7, 8, 1, 3, 5].
+
+
+
+Now, your task is to find the index at which ‘k’ is present in 'arr'.
+
+
+*/
 
 class Solution {
 public:
