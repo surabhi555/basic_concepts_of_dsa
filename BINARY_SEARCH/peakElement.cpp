99d785fa05fe6263b9f79c316cde55567acfd823
surabhi555 <pednekarsurabhi555@gmail.com>
2024-11-30 21:31:16 +0530

    done

--- BINARY_SEARCH/peakElement.cpp
+++ BINARY_SEARCH/peakElement.cpp
@@ -1,3 +1,14 @@
+/* You are given an integer mountain array arr of length n where the values increase to a peak element and then decrease.
+
+Return the index of the peak element.
+
+Your task is to solve it in O(log(n)) time complexity.
+*/
+
+
+
+
+
 #include<bits/stdc++.h>
 using namespace std;
 int peakIndex(int arr[],int n){
