4248aad89c3eb369f025a5099879ecf40266fcad
surabhi555 <pednekarsurabhi555@gmail.com>
2024-12-05 10:29:00 +0530

    done

--- README.md
+++ README.md
@@ -1,9 +1,3 @@
 
 #solved
-1.sorted and rotated array
-2.roatate array
-3.sum of two array
-#strings
-1.length of string
-2.reverse string
-3.check if sstring is palimdrome or not
+1.max occurences in string
---
+++ Strings/maxOcc.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+char getMaxOccuringChar(string& s) {
+      
+        // int arr[26]={0};
+        int arr[26]={0};
+        
+        //count
+        int number=0;
+        for(int i=0;i<s.length();i++){
+            char ch=s[i];
+             number=ch-'a';
+             arr[number]++;
+        }
+        int maxi=-1;
+        int ans=0;
+        for(int i=0;i<26;i++){
+            if(maxi < arr[i]){
+                ans=i;
+                maxi=arr[i];
+            }
+        }
+        return 'a'+ans;
+    }
+    int main() {
+    // Test cases
+    string input1 = "examplestring";
+    string input2 = "hello";
+    string input3 = "character";
+
+    // Call the function and print the results
+    cout << "Input: " << input1 << ", Most occurring character: " << getMaxOccuringChar(input1) << endl;
+    cout << "Input: " << input2 << ", Most occurring character: " << getMaxOccuringChar(input2) << endl;
+    cout << "Input: " << input3 << ", Most occurring character: " << getMaxOccuringChar(input3) << endl;
+
+    return 0;
+}
