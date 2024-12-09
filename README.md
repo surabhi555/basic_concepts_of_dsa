e3e5b6f15234a6822e27d055279d08905229ef9f
surabhi555 <pednekarsurabhi555@gmail.com>
2024-12-09 11:06:34 +0530

    remove substring

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 
 #solved
 1.replace space in string with @40
+2.remove substring
---
+++ Strings/removeOccSubsdtring.cpp
@@ -0,0 +1,41 @@
+/*
+Given two strings s and part, perform the following operation on s until all occurrences of the substring part are removed:
+
+Find the leftmost occurrence of the substring part and remove it from s.
+Return s after removing all occurrences of part.
+
+A substring is a contiguous sequence of characters in a string.
+
+ 
+
+Example 1:
+
+Input: s = "daabcbaabcbc", part = "abc"
+Output: "dab"
+Explanation: The following operations are done:
+- s = "daabcbaabcbc", remove "abc" starting at index 2, so s = "dabaabcbc".
+- s = "dabaabcbc", remove "abc" starting at index 4, so s = "dababc".
+- s = "dababc", remove "abc" starting at index 3, so s = "dab".
+Now s has no occurrences of "abc".
+
+ */
+#include<bits/stdc++.h>
+using namespace std;
+string removeOccurence(string s,string part){
+    while(s.length()!=0 && s.find(part)<s.length()){
+        s.erase(s.find(part),part.length());
+    }
+    return s;
+}
+int main() {
+    string s, part;
+    cout << "Enter the main string: ";
+    cin >> s;
+    cout << "Enter the substring to remove: "<<endl;
+    cin >> part;
+
+    string result = removeOccurence(s, part);
+    cout << "Modified String: " << result << endl;
+
+    return 0;
+}
